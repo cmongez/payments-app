b85da249cc81807bcfa400fc4ad2a8d612a3c627
Cesar <camongez28@gmail.com>
2022-10-06 20:01:24 -0300

    modelo de datos implementado, funcion addPayment añadida

--- src/components/PaymentsApp.vue
+++ src/components/PaymentsApp.vue
@@ -6,13 +6,13 @@
       ><span>{{ this.CURRENCY }}</span>
     </div>
     <div class="d-flex">
-      <div class="card">
+      <div v-for="(item, index) in db" :key="index" class="card">
         <div><div class="circle">circulo</div></div>
-        <p>{{ title }}</p>
-        <span>{{ this.RECEIVABLE }} {{ this.CURRENCY }} ({{ this.porcent }})%</span>
+        <p>{{ item.title }}</p>
+        <span>{{ RECEIVABLE }} {{ CURRENCY }} ({{ item.percentage }})%</span>
         <div>22 Ene, 2022</div>
       </div>
-      <div><button>+</button></div>
+      <div><button @click="addPayment">+</button></div>
     </div>
   </div>
 </template>
@@ -28,8 +28,26 @@ export default {
       CURRENCY,
       RECEIVABLE,
 
-      db: [{ title: 'Anticipo', porcent: 100, instalments: 1 }], //Cuotas
+      db: [{ id: 1, title: 'Anticipo', percentage: 100, instalments: 1 }], //Cuotas
     };
+  },
+
+  methods: {
+    addPayment() {
+      let lastPayment = this.db[this.db.length - 1];
+      console.log(lastPayment.title);
+      lastPayment.title == 'Anticipo' ? lastPayment.title : (lastPayment.title = `Pago ${lastPayment.id}`);
+      let newPayment = {
+        id: this.db.length,
+        title: 'Pago Final',
+        percentage: lastPayment.percentage / 2,
+        instalments: 1,
+      };
+      lastPayment.percentage /= 2;
+
+      this.db.push(newPayment);
+      console.log(this.db);
+    },
   },
 };
 </script>
